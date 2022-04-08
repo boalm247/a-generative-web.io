f8b21f688635974ba59b772af06840ce94a585e1
boalm247 <60275424+boalm247@users.noreply.github.com>
2022-04-08 09:51:29 -0400

    Add all parts to weaving generator

--- weaving/weavingPattern.js
+++ weaving/weavingPattern.js
@@ -1,5 +1,7 @@
 function setup() {
-    createCanvas(1000, 600);
+    let w = window.innerWidth;
+    let h = window.innerHeight + 700;
+    createCanvas(w, h);
     noLoop();
     noStroke();
 }
@@ -15,17 +17,22 @@ function draw() {
     //distance outside sqaures
     var oMargin = 10;
     //grid width and height outside sqaures
-    var gw = 4;
-    var gh = 22;
+    var gridwidtha = 4;
+    var gridwidthb =  6;
+    var gridheighta = 27;
+    var gridheightb = 17;
+
 
     // two for loops are needed to make a grid of squares
     // the outer loop controls how many rows are made
     // the inner loop controls how many columns are made
-    for (let y = 0; y < gw; y++) {
-        for (let x = 0; x < gh; x++) {
+
+    //this is the top horizontal grid (threadling)
+    for (let y = 0; y < gridwidtha; y++) {
+        for (let x = 0; x < gridheighta; x++) {
             rect(x * margin + oMargin, y * margin + oMargin, size, size);
-            if (Math.random() > .5) {
-                fill(200, 200, 200);
+            if (Math.random() > .45) {
+                fill(217, 217, 217);
 
             }
             else{
@@ -35,7 +42,48 @@ function draw() {
 
         }
     }
-}
+
+    //this is the side vertical grid (treadling)
+    for (let y = 0; y < gridheightb; y++) {
+        for (let x = 0; x < gridwidtha; x++) {
+            rect(x * margin + oMargin + 1100, y * margin + oMargin + 175, size, size);
+            if (Math.random() > .25) {
+                fill(217, 217, 217);
+            }
+            else {
+                fill(0, 0, 0);
+            }
+        }
+    }
+    //this is the small top box (tie up)
+    for (let y = 0; y < gridwidtha; y++){
+        for (let x = 0; x < gridwidthb; x++){
+            rect(x * margin + oMargin + 1100, y * margin + oMargin, size, size);
+            if (Math.random() > .3){
+                fill(217, 217, 217);
+
+            }else {
+                fill(0, 0, 0);
+            }
+            
+        }
+    }
+
+    //this is the large left box (weave pattern)
+    for (let y = 0; y < gridheightb; y++){
+        for (let x = 0; x < gridheighta; x++){
+            fill(217, 217, 217)
+            rect(x * margin + oMargin, y * margin + oMargin + 175, size, size);
+        }
+
+    }
+
+//draw function end
+} 
+
+
+
+
 
 
 
