11021b436b48c3212b12daa357ad8c8030515dd5
boalm247 <60275424+boalm247@users.noreply.github.com>
2022-04-13 12:31:46 -0400

    revert version 1 to original form

--- weaving040422/weavingPattern.js
+++ weaving040422/weavingPattern.js
@@ -17,19 +17,17 @@ function draw() {
     //distance outside sqaures
     var oMargin = 10;
     //grid width and height outside sqaures
-    var gridwidtha = 4;
-    var gridwidthb =  6;
-    var gridheighta = 27;
-    var gridheightb = 17;
+    var gridwidth = 4;
+    var gridheight = 27;
 
 
     // two for loops are needed to make a grid of squares
     // the outer loop controls how many rows are made
     // the inner loop controls how many columns are made
 
-    //this is the top horizontal grid (threadling)
-    for (let y = 0; y < gridwidtha; y++) {
-        for (let x = 0; x < gridheighta; x++) {
+
+    for (let y = 0; y < gridwidth; y++) {
+        for (let x = 0; x < gridheight; x++) {
             rect(x * margin + oMargin, y * margin + oMargin, size, size);
             if (Math.random() > .45) {
                 fill(217, 217, 217);
@@ -44,40 +42,9 @@ function draw() {
         }
     }
 
-    //this is the side vertical grid (treadling)
-    for (let y = 0; y < gridheightb; y++) {
-        for (let x = 0; x < gridwidthb; x++) {
-            rect(x * margin + oMargin + 1100, y * margin + oMargin + 175, size, size);
-            if (Math.random() > .25) {
-                fill(217, 217, 217);
-            }
-            else {
-                fill(0, 0, 0);
-            }
-        }
-    }
-    //this is the small top box (tie up)
-    for (let y = 0; y < gridwidtha; y++){
-        for (let x = 0; x < gridwidthb; x++){
-            rect(x * margin + oMargin + 1100, y * margin + oMargin, size, size);
-            if (Math.random() > .25){
-                fill(217, 217, 217);
 
-            }else {
-                fill(0, 0, 0);
-            }
-            
-        }
-    }
 
-    //this is the large left box (weave pattern)
-    for (let y = 0; y < gridheightb; y++){
-        for (let x = 0; x < gridheighta; x++){
-            fill(217, 217, 217)
-            rect(x * margin + oMargin, y * margin + oMargin + 175, size, size);
-        }
-
-    }
+ 
 
 //draw function end
 } 
